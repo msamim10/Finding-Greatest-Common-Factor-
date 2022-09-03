d067368a54aaadae909a91d84d92a4db01c2548f
msamim10 <94254181+msamim10@users.noreply.github.com>
2022-09-03 17:04:11 -0400

    Add files via upload

---
+++ GCD.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+// Output GCD of user-input numA and numB
+
+  class GCDCalc {
+   public static void main(String[] args) {
+      Scanner scnr = new Scanner(System.in);
+      int numA;  // User input
+      int numB;  // User input
+
+      System.out.print("Enter first positive integer: ");
+      numA = scnr.nextInt();
+
+      System.out.print("Enter second positive integer: ");
+      numB = scnr.nextInt();
+
+      while (numA != numB) { // Euclid's algorithm
+         if (numB > numA) {
+            numB = numB - numA;
+         }
+         else {
+            numA = numA - numB;
+         }
+      }
+
+      System.out.println("GCD is: " + numA);
+   }
+}
